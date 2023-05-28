6d12c2b3bbe51ef5c910fac8453bacba85a2e66e
jokereven <zjing125.cn@gmail.com>
2023-05-28 16:02:38 +0800

    feat: init flare_contract

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard OKTC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://exchaintestrpc.okex.org`),
+      network_id: 65,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+    }
+  }
+}
